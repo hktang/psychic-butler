51b49df176ba7fc6e7cf607a342ae47d3efb247a
Xiaojun Deng <hiukuan.tang@gmail.com>
2017-12-31 14:13:50 +0900

    Setup Vue system.

---
+++ resources/assets/js/components/TaskItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <td class="task-item">
+        I am a task item.
+        <button type="button" class="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </td>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component TaskItem mounted.')
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Tasks.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h1>Tasks</h1></div>
+
+                    <div class="panel-body">
+                    </div>
+
+                    <table class="table">
+                        <tbody>
+                            <tr v-for="n in 5">
+                                <task-item></task-item>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TaskItem from './TaskItem.vue'
+    export default {
+        components: {
+            TaskItem
+            },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
